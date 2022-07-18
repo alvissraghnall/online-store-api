e35b97e86c8f5215cd88e072aefab8d1f84e7c00
Alviss Raghnall <isiomastewart@gmail.com>
2022-07-18 06:16:46 +0100

    Lupe Fiasco - MURAL

---
+++ src/application.ts
@@ -0,0 +1,59 @@
+import {BootMixin} from '@loopback/boot';
+import {ApplicationConfig} from '@loopback/core';
+import {GraphQLBindings, GraphQLComponent} from '@loopback/graphql';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent
+} from '@loopback/rest-explorer';
+import {ServiceMixin} from '@loopback/service-proxy';
+import path from 'path';
+import {sampleRecipes} from './sample-recipes';
+import {MySequence} from './sequence';
+
+export {ApplicationConfig};
+
+export class App extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    super(options);
+
+    this.sequence(MySequence);
+
+    this.component(GraphQLComponent);
+    const server = this.getSync(GraphQLBindings.GRAPHQL_SERVER);
+    this.expressMiddleware('middleware.express.GraphQL', server.expressApp);
+
+    // It's possible to register a graphql context resolver
+    this.bind(GraphQLBindings.GRAPHQL_CONTEXT_RESOLVER).to(context => {
+      return {...context};
+    });
+
+    this.bind('recipes').to([...sampleRecipes]);
+
+    this.static('/', path.join(__dirname, '../public'));
+
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
+
+    this.projectRoot = __dirname;
+
+    this.bootOptions = {
+      controllers: {
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+      graphqlResolvers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['graphql-resolvers'],
+        extensions: ['.js'],
+        nested: true,
+      },
+    };
+  }
+}
