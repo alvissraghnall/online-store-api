21fdf6d5f26349b0f2b4ef431174ff9f1165688a
Alviss Raghnall <isiomastewart@gmail.com>
2023-07-14 13:54:59 +0100

    Alan Walker -All Falls Down

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -2,7 +2,7 @@ import {authenticate} from '@loopback/authentication'
 import {UserRepository} from '@loopback/authentication-jwt'
 import {inject} from '@loopback/context'
 import {service} from '@loopback/core'
-import {post, getModelSchemaRef, response, requestBody} from '@loopback/openapi-v3'
+import {post, getModelSchemaRef, response, requestBody, SchemaObject} from '@loopback/openapi-v3'
 import {repository} from '@loopback/repository'
 import {SecurityBindings, UserProfile} from '@loopback/security';
 import {Product, User} from '../models'
@@ -13,24 +13,69 @@ type PaymentDetails = {
   items: Product[]
 }
 
+const paystackTxnInitResponseSchema: SchemaObject = {
+  $schema: "http://json-schema.org/draft-04/schema#",
+  title: "paystackTxnInitResponse",
+  type: "object",
+  properties: {
+    status: {
+      type: "boolean",
+    },
+    message: {
+      type: "string",
+    },
+    data: {
+      type: "object",
+      properties: {
+        authorization_url: {
+          type: "string",
+        },
+        access_code: {
+          type: "string",
+        },
+        reference: {
+          type: "string",
+        },
+      },
+      required: ["authorization_url", "access_code", "reference"],
+    },
+  },
+  required: ["status", "message", "data"],
+};
 
 
 @authenticate('jwt')
 export class PaymentController {
   constructor(
-    @service(PaymentService) private favouritesService: PaymentService,
+    @service(PaymentService) private paymentService: PaymentService,
     @inject(SecurityBindings.USER) private loggedInUserProfile: UserProfile,
   ) {}
 
   @post('/payment')
   @response(200, {
-    description: 'User model instance',
-    content: {'application/json': {schema: {
-      p
-    }}},
+    description: 'Payment initialization response',
+    content: {'application/json': {schema: paystackTxnInitResponseSchema }},
   })
-  pay () {
-
+  pay (
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: {
+            $schema: "http://json-schema.org/draft-04/schema#",
+            title: "req body for payment method",
+            type: "object",
+            properties: {
+              amount: {
+                title: "total amount",
+                type: "number",
+              }
+            }
+          }
+        }
+      }
+    }) amount: number
+  ) {
+    return this.paymentService.makePayment(amount);
   }
 
 }
--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -8,16 +8,30 @@ import {UserRepository} from '../repositories/user.repository';
 import {SecurityBindings, UserProfile, securityId} from '@loopback/security';
 import {OrderService} from './order.service';
 import axios from 'axios';
+import {CartService} from './cart.service';
 
 type PaystackParams = {
-  email: string,
-  key: string, amount: number,
+  email: string, amount: number,
   currency?: 'NGN' | 'GHS' | 'ZAR' | 'USD',
   channels?: ['card', 'bank', 'ussd', 'qr', 'mobile_money', 'bank_transfer'],
   metadata?: any,
-  callback?: (response: {reference: string, access_code: string, authorization_url: string}) => any,
-  onClose?: () => any
+  callback_url?: string,
+  // onClose?: () => any
 }
+
+interface PaystackTxnInitResponse {
+  status: boolean;
+  message: string;
+  data: PaystackTxnInitData;
+}
+
+interface PaystackTxnInitData {
+  authorization_url: string;
+  access_code: string;
+  reference: string;
+}
+
+// response: {reference: string, access_code: string, authorization_url: string}
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class PaymentService {
@@ -27,7 +41,7 @@ export class PaymentService {
       baseURL: 'https://api.paystack.co',
       method: 'POST',
       headers: {
-        Authorization: process.env.PAYSTACK_TEST_SECRET,
+        Authorization: `Bearer ${process.env.PAYSTACK_TEST_SECRET}`,
         'Content-Type': 'application/json'
       }
     }
@@ -35,25 +49,40 @@ export class PaymentService {
 
   constructor(
     @service(OrderService) private readonly orderService: OrderService,
+    @service(CartService) private readonly cartService: CartService,
     @inject(SecurityBindings.USER) private loggedInUserProfile: UserProfile,
     @repository(UserRepository) private readonly userRepository: UserRepository,
 
   ) {}
 
-  async makePayment () {
+  async makePayment (amountToPay: number) {
+    console.log(amountToPay);
     const currUser = await this.userRepository.findById(
       this.loggedInUserProfile[securityId]
     );
-    const { email, firstName, lastName } = currUser;
+    const cart = await this.cartService.findByUser(this.loggedInUserProfile, false);
+    const amount = cart.items.reduce(
+      (acc, curr) => acc += (curr.product.price * curr.quantity),
+      0
+    ) * 100; //multiplying by 100 to get value in cents.
 
-    const res = await this.axiosInstance.post(
+    const data: PaystackParams = {
+      email: currUser.email,
+      amount,
+      currency: "USD",
+      callback_url: 'http://localhost:4000/prior-to-order',
+      metadata: JSON.stringify(cart.items)
+    }
+
+    const res = await this.axiosInstance.post<PaystackTxnInitResponse>(
       "/transaction/initialize",
-      {email, name: `${firstName} ${lastName}`},
+      data,
     ).catch(
       err => HttpErrors[500]("Something went wrong from our end.")
     );
 
     console.log(res);
+    return res.data;
   }
 
 }
